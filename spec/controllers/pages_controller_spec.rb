8bbedced40f712907f145270ffeaad0cb1a49476
Jorge Paez <george.paez@gmail.com>
2010-10-24 18:04:12 -0500

    Added a User Controller

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -6,17 +6,13 @@ describe PagesController do
   before(:each) do
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
-
+        
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
-    it "should the rigth title" do
-      get 'home'
-      response.should have_selector("title",
-                                    :content =>"#{@base_title} | Home")
-    end
+
     it "should have a non-blank body"do
       get "home"
       response.body.should_not =~ /<body>\s*<\/body>/
